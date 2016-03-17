83d592c413a2b36699308dee9e6c161ea57b34f5
Jon Ringvald <jvald8@gmail.com>
2016-03-17 12:56:22 -0400

    restructure into dist and app directories, add .babelrc, webpack

---
+++ app/index.js
@@ -0,0 +1,14 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var HelloWorld = React.createClass({
+  render: function(){
+    return (
+      <p>
+        Hello World!
+      </p>
+    )
+  }
+});
+
+ReactDOM.render(<HelloWorld />, document.getElementById('hello_world'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,23 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/app/complex.html',
+  filename: 'complex.html',
+  inject: 'body'
+});
+
 module.exports = {
 	entry: [
 		'./app/index.js'
 	],
 	module: {
-		loaders: []
-	}
-}
+		loaders: [
+			{test: /\.js$/, include: __dirname + '/app', loader: "babel-loader"}
+		]
+	},
+	output: {
+		filename: "index_bundle.js",
+		path: __dirname + '/dist'
+	},
+	plugins: [HTMLWebpackPluginConfig]
+};
